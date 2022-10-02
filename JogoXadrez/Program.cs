c584165467841f15834084542dd0a1bf94171ef3
Mateus Nascimento <mateus.nascimento1006@gmail.com>
2022-10-02 19:09:48 -0300

    Add project files.

---
+++ JogoXadrez/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace JogoXadrez
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
