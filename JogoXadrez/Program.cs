f2f92f498d6428c17d7ff3ee1f7691aa277e876b
Mateus Nascimento <mateus.nascimento1006@gmail.com>
2022-10-03 17:13:43 -0300

    Criada a classe que converte a posição do xadrez para matriz

--- JogoXadrez/Program.cs
+++ JogoXadrez/Program.cs
@@ -6,26 +6,16 @@ namespace JogoXadrez
     {
         static void Main(string[] args)
         {
-            try
-            {
-                Tabuleiro tab = new Tabuleiro(8, 8);
+            PosicaoXadrez pos = new PosicaoXadrez('c', 7);
 
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 10));
-                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 2));
-
-                Tela.imprimirTabuleiro(tab);
-
-                Console.ReadLine();
-            }
-            catch (TabuleiroException e)
-            {
-                Console.WriteLine("Error: " + e.Message);
-                
-            }
-            
+            Console.WriteLine(pos.toPosicao());
 
 
+
+
+
+
+            Console.ReadLine();
         }
     }
 }
