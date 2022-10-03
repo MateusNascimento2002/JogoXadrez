6e6d5b7ebfc7f0df01cbe48e4a2a4710a234d986
Mateus Nascimento <mateus.nascimento1006@gmail.com>
2022-10-03 17:02:39 -0300

    Implementação dos tratamentos de erro com Exception personalizada.

--- JogoXadrez/Program.cs
+++ JogoXadrez/Program.cs
@@ -6,29 +6,24 @@ namespace JogoXadrez
     {
         static void Main(string[] args)
         {
+            try
+            {
+                Tabuleiro tab = new Tabuleiro(8, 8);
 
-            Tabuleiro tab = new Tabuleiro(8, 8);
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 10));
+                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 2));
 
-            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-            tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
+                Tela.imprimirTabuleiro(tab);
 
-
-
-
-
-
-
-
-
-
-
-
-            Tela.imprimirTabuleiro(tab);
-
-
-
-            Console.ReadLine();
+                Console.ReadLine();
+            }
+            catch (TabuleiroException e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+                
+            }
+            
 
 
         }
