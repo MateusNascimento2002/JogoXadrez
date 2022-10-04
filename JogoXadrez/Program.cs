0664b519815503fce844746290f34bb845a464b7
Mateus Nascimento <mateus.nascimento1006@gmail.com>
2022-10-04 17:04:27 -0300

    Preparado o projeto para os teste inicias, implementada a logica de impressão da pela na posição do xadrez e implementador metodo de mover a peça da origem ao destino.

--- JogoXadrez/Program.cs
+++ JogoXadrez/Program.cs
@@ -9,21 +9,14 @@ namespace JogoXadrez
 
             try
             {
-                Tabuleiro tab = new Tabuleiro(8, 8);
+                PartidaDeXadrez partida = new PartidaDeXadrez();
 
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 4));
-                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 5));
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 6));
-
-                tab.colocarPeca(new Torre(tab, Cor.Branca), new Posicao(5, 4));
-                tab.colocarPeca(new Rei(tab, Cor.Branca), new Posicao(5, 5));
-                tab.colocarPeca(new Torre(tab, Cor.Branca), new Posicao(5, 6));
-
-                Tela.imprimirTabuleiro(tab);
+               
+                Tela.imprimirTabuleiro(partida.tab);
             }
             catch (TabuleiroException e)
             {
-                Console.WriteLine(e);
+                Console.WriteLine(e.Message);
                 throw;
             }
 
