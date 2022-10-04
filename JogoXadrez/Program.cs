bc1177fcce401bb06970d506898fcf38c64250bb
Mateus Nascimento <mateus.nascimento1006@gmail.com>
2022-10-04 16:11:48 -0300

    Implementado linhas e colunas de referência do tabuleiro e metodo para trocar a cor da peça preta.

--- JogoXadrez/Program.cs
+++ JogoXadrez/Program.cs
@@ -6,11 +6,26 @@ namespace JogoXadrez
     {
         static void Main(string[] args)
         {
-            PosicaoXadrez pos = new PosicaoXadrez('c', 7);
 
-            Console.WriteLine(pos.toPosicao());
+            try
+            {
+                Tabuleiro tab = new Tabuleiro(8, 8);
 
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 4));
+                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 5));
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 6));
 
+                tab.colocarPeca(new Torre(tab, Cor.Branca), new Posicao(5, 4));
+                tab.colocarPeca(new Rei(tab, Cor.Branca), new Posicao(5, 5));
+                tab.colocarPeca(new Torre(tab, Cor.Branca), new Posicao(5, 6));
+
+                Tela.imprimirTabuleiro(tab);
+            }
+            catch (TabuleiroException e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
 
 
 
